09f8c1c2ee62c54248be3374f3d806d760bc3db4
Ayoub <ayo.daif@gmail.com>
2023-04-03 17:23:06 +0200

    add dev environment

--- modules/hello-world/variables.tf
+++ modules/hello-world/variables.tf
@@ -5,3 +5,9 @@ variable "key_name" {
   type    = string
   default = "my_key_pairs"
 } 
+
+variable "environment_name" {
+  description = "Deployment environment (dev/staging/production)"
+  type        = string
+  default     = "dev"
+}
