8fc0f6adaad4c194a0527e3853afac82503db0b9
Ayoub <ayo.daif@gmail.com>
2023-04-03 17:44:38 +0200

    adjust dev env

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,17 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
 
 locals {
   environment_name = "dev"
@@ -12,4 +24,5 @@ module "web_app" {
   # Input Variables
   key_name         = var.key_name
   environment_name = local.environment_name
+  region           = var.region
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,3 +2,9 @@
 variable "key_name" {
   type = string
 }
+
+variable "region" {
+  description = "aws region"
+  type = string
+  default = "eu-west-3"
+}
